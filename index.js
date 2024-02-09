24ebcbc4b6095df985a0411541c7fc600f26e93c
RavinduSM <70681852+RavinduSM@users.noreply.github.com>
2024-02-09 23:34:34 +0530

    Create a middleware and a function to handle possible errors

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`)
 });
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+})
